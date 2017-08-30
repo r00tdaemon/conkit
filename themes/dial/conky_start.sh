9964f6d1515741eae982ef77548938e356b4e3df
Ujjwal Verma <ujjwalverma1111@gmail.com>
2017-08-31 05:04:12 +0530

    Daemonize conky processes

--- themes/dial/conky_start.sh
+++ themes/dial/conky_start.sh
@@ -2,9 +2,9 @@
 
 cd "$(dirname "$0")"
 
-conky -c ./conkyrc &
-conky -c ./horical_conky &
-conky -c ./ip_conkyrc &
-conky -c ./net_conkyrc &
-conky -c ./info_conkyrc &
-conky -c ./top_conkyrc
+conky -d -c ./conkyrc &
+conky -d -c ./horical_conky &
+conky -d -c ./ip_conkyrc &
+conky -d -c ./net_conkyrc &
+conky -d -c ./info_conkyrc &
+conky -d -c ./top_conkyrc
--- themes/equalizer/conky_start.sh
+++ themes/equalizer/conky_start.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 cd "$(dirname "$0")"
-conky -c ./conkyrc &
-conky -c ./horical_conky &
-conky -c ./ip_conkyrc &
-conky -c ./net_conkyrc &
-conky -c ./info_conkyrc &
-conky -c ./top_conkyrc
+conky -d -c ./conkyrc &
+conky -d -c ./horical_conky &
+conky -d -c ./ip_conkyrc &
+conky -d -c ./net_conkyrc &
+conky -d -c ./info_conkyrc &
+conky -d -c ./top_conkyrc
